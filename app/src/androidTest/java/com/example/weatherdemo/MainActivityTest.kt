8c21fd2faf5ef303dc12d59405f439df087df894
mark.yang <mark.yang@mintpayments.com>
2020-07-21 10:59:44 +1000

    Added instrument test - activity

---
+++ app/src/androidTest/java/com/example/weatherdemo/MainActivityTest.kt
@@ -0,0 +1,53 @@
+package com.example.weatherdemo
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest{
+
+    private lateinit var stringToBetyped: String
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity>
+            = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun initValidString() {
+        stringToBetyped = "Espresso"
+    }
+
+    @Test
+    fun testEvent() {
+
+        onView(withId(R.id.weather_updated)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_day_of_today)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_temp)).check(matches(isDisplayed()))
+        onView(withId(R.id.feelslike)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_feellike)).check(matches(isDisplayed()))
+        onView(withId(R.id.rain_drops)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_rain)).check(matches(isDisplayed()))
+        onView(withId(R.id.wind)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_wind)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_today)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_icon)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_desc)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_temp_max)).check(matches(isDisplayed()))
+        onView(withId(R.id.weather_temp_min)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.bottomSheet)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.activity_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.activity_search)).perform(click())
+
+    }
+}
